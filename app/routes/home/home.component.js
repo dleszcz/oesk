84c188b9a752999032d3fcf35683225842f7cd12
Daniel Leszczyński <danielleszczynski@MacBook-Pro-Daniel.local>
2018-01-19 23:56:42 +0100

    Wait for metrics

--- app/routes/home/home.component.js
+++ app/routes/home/home.component.js
@@ -2,10 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
-import envConfig from 'env-config';
-import { Link } from 'react-router-dom';
 import Perfume from 'perfume.js';
-
 import messages from './home.messages';
 import { ItemsList } from './itemsList/itemsList.component';
 import { LanguageSelector } from './languageSelector/languageSelector.component';
@@ -28,7 +25,6 @@ export class Home extends PureComponent {
   state = {
     loaded: false,
     mountingComponent: 0,
-    painting: 0,
     fetchingData: 0,
     firstContentfulPaintDuration: 0,
     timeToInteractiveDuration: 0,
@@ -39,19 +35,14 @@ export class Home extends PureComponent {
     this.perfume.start('Painting');
     this.perfume.start('FetchingData');
     this.props.fetchMaintainers(this.props.language);
+    this.waitForCFPInterval = this.waitForCFP();
+    this.waitForTTIInterval = this.waitForTTI();
   }
 
   componentDidMount() {
     this.props.setCurrentResult({ data: 'data' });
     const mountingComponent = this.perfume.end('MountingComponent');
-    const { firstContentfulPaintDuration, timeToInteractiveDuration } = this.perfume;
-
-    this.setState({
-      mountingComponent,
-      firstContentfulPaintDuration,
-      timeToInteractiveDuration,
-    });
-    console.log('metrics', this.state);
+    this.setState({ mountingComponent });
   }
 
   componentWillReceiveProps(nextProps) {
@@ -67,18 +58,37 @@ export class Home extends PureComponent {
     }
   }
 
+  waitForCFPInterval = null;
+  waitForTTIInterval = null;
   perfume = new Perfume({
     logging: true,
     firstContentfulPaint: true,
     timeToInteractive: true,
   });
 
-  render() {
-    if (!this.props.items.size) {
-      return <div>Loading...</div>;
-    }
+  waitForTTI = () => setInterval(() => {
+      console.log('waiting for tti');
+      if (this.perfume.timeToInteractiveDuration !== 0) {
+        this.setState({
+          timeToInteractiveDuration: this.perfume.timeToInteractiveDuration,
+        });
+        clearInterval(this.waitForTTIInterval);
+      }
+  }, 5000);
 
-    const painting = this.perfume.endPaint('Painting');
+  waitForCFP = () => setInterval(() => {
+      console.log('waiting for cfp');
+      if (this.perfume.firstContentfulPaintDuration !== 0) {
+        this.setState({
+          firstContentfulPaintDuration: this.perfume.firstContentfulPaintDuration
+        });
+        clearInterval(this.waitForCFPInterval);
+      }
+  }, 2500);
+
+  render() {
+
+    const { mountingComponent, fetchingData, firstContentfulPaintDuration, timeToInteractiveDuration } = this.state;
 
     return (
       <div className="home">
@@ -89,9 +99,29 @@ export class Home extends PureComponent {
           <FormattedMessage {...messages.welcome} />
         </h1>
 
-        <div>
-          <Link to={`${this.props.match.url}/results`}>Go to results</Link>
+        <div className="home__results">
+          <h2>Your results:</h2>
+          <div>
+            <span className="home__results-title">Mounting Component:</span>
+            <span className="home__results-value">{mountingComponent ? mountingComponent : '...'}</span>
+          </div>
+          <div>
+            <span className="home__results-title">Fetching API data:</span>
+            <span className="home__results-value">{fetchingData ? fetchingData : '...'}</span>
+          </div>
+          <div>
+            <span className="home__results-title">First Contentful Paint:</span>
+            <span className="home__results-value">{firstContentfulPaintDuration ? firstContentfulPaintDuration : '...'}</span>
+          </div>
+          <div>
+            <span className="home__results-title">Time To Interactive:</span>
+            <span className="home__results-value">{timeToInteractiveDuration ? timeToInteractiveDuration : '...'}</span>
+          </div>
         </div>
+
+        { !this.props.items.size
+          ? <span className="home__loading-status home__loading-status--waiting">Loading content...</span>
+          : <span className="home__loading-status home__loading-status--loaded">Loaded content</span> }
 
         <ItemsList items={this.props.items} />
 
